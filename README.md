44591cfcf079e0718343f382605871444f358027
kimiszero <zerokim0215@gmail.com>
2021-02-26 14:46:15 +0900

    create_frontpage::join/login

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# my project :: 김은영
+# project name :: zerofit
+### HOMEFIT 클론코딩 [2021.02 - 2021. ??]
+
+![project_zerofit](https://user-images.githubusercontent.com/77275707/109255039-8f23fe80-7836-11eb-9c09-18deaaffb3f9.png)
+
+* 기능 
+1. 리뷰 게시판 (페이지네이션)
+2. 회원가입 | 로그인
+3. 수업신청하기 
--- zerofit/app.py
+++ zerofit/app.py
@@ -14,9 +14,15 @@ def home():
 @app.route('/api/list', methods=['GET'])
 def show_reviews():
     reviews = list(db.reviews.find({}, {'_id': False}))
-
-    # 2. 성공하면 success 메시지와 함께 stars_list 목록을 클라이언트에 전달합니다.
     return jsonify({'result': 'success', 'reviews': reviews})
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/join')
+def join():
+    return render_template('join.html')
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
